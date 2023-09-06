6b3011eddef38de16923e97b3d170c2dbde57a5e
Ruth Dantas <ruthdevgp@gmail.com>
2023-09-06 17:09:15 -0300

    subindo exercício 02 de loops

---
+++ java/src/loops_arrays/exercicios/loops/Exer02_Nota.java
@@ -0,0 +1,19 @@
+package loops_arrays.exercicios.loops;
+
+import java.util.Scanner;
+
+public class Exer02_Nota {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Digite uma nota: ");
+        nota = scanner.nextInt();
+
+        while (nota < 0 | nota > 10) {
+            System.out.println("Nota inválida. Digite uma nota de 0 a 10: ");
+            nota = scanner.nextInt();
+        }
+    }
+}
