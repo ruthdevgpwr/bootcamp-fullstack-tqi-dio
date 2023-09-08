538ae12ab1f6a971872145148276060aab63cb3d
Ruth Dantas <ruthdevgp@gmail.com>
2023-09-08 14:26:43 -0300

    Subindo exercício 02 - Arrays

---
+++ java/src/loops_arrays/exercicios/arrays/Exer02_Consoantes.java
@@ -0,0 +1,37 @@
+package loops_arrays.exercicios.arrays;
+
+import java.util.Scanner;
+
+public class Exer02_Consoantes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeConsoantes = 0;
+
+        int count = 0;
+        do {
+            System.out.println("Digite uma letra: ");
+            String letra = scanner.next();
+
+            if (!(letra.equalsIgnoreCase("a") |
+                    letra.equalsIgnoreCase("e") |
+                    letra.equalsIgnoreCase("i") |
+                    letra.equalsIgnoreCase("o") |
+                    letra.equalsIgnoreCase("u"))
+            ) {
+                consoantes[count] = letra;
+                quantidadeConsoantes++;
+            }
+
+            count++;
+        } while (count < consoantes.length);
+
+        System.out.print("Consoantes: ");
+        for (String consoante : consoantes ) {
+            if (consoante != null)
+                System.out.println(consoante + " ");
+        }
+        System.out.println("Quantidade de consoantes: " + quantidadeConsoantes);
+    }
+}
